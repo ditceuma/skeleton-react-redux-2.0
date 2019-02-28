c86580d8e01d5f554024560345ff6ffcd0a4810b
Marcus Cartágenes <mvcartagenes@gmail.com>
2019-02-28 10:55:20 -0300

    (src) Using react hooks on (#1)
    Accept!

--- src/App.js
+++ src/App.js
@@ -1,66 +1,48 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Logo } from "./Components";
 import { decreaseRate, increaseRate } from "./Actions/appActions";
 import { connect } from "react-redux";
 import "./Assets/css/App.css";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      rate: 0
-    };
-    this.increase = this.increase.bind(this);
-    this.decrease = this.decrease.bind(this);
-  }
+const handleControlRate = (plus,value, {decreaseRate, increaseRate} ) => plus ? increaseRate(value) : decreaseRate(value)
 
-  componentWillReceiveProps(props) {
-    this.setState({ rate: props.rate });
-  }
-
-  increase() {
-    this.props.increaseRate(this.state.rate);
-  }
-
-  decrease() {
-    this.props.decreaseRate(this.state.rate);
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <Logo />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <p>Sua nota para essa pagina: {this.props.rate}</p>
-          <button
-            target="_blank"
-            onClick={this.increase}
-            rel="noopener noreferrer"
-          >
-            Aumentar nota
-          </button>
-          <button
-            target="_blank"
-            onClick={this.decrease}
-            rel="noopener noreferrer"
-          >
-            Diminuir nota
-          </button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const App = (props) => {
+  const [rate, setRate] = useState(0)
+  useEffect(() => setRate(props.rate))
+  
+  return(
+    <div className="App">
+      <header className="App-header">
+        <Logo />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>Sua nota para essa pagina: {props.rate}</p>
+        <button
+          target="_blank"
+          onClick={() => handleControlRate(true,rate,props)}
+          rel="noopener noreferrer"
+        >
+          Aumentar nota
+        </button>
+        <button
+          target="_blank"
+          onClick={() => handleControlRate(false,rate,props)}
+          rel="noopener noreferrer"
+        >
+          Diminuir nota
+        </button>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
 }
 
 const mapStateToProps = state => ({
@@ -68,6 +50,5 @@ const mapStateToProps = state => ({
 });
 
 export default connect(
-  mapStateToProps,
-  { decreaseRate, increaseRate }
+  mapStateToProps, { decreaseRate, increaseRate }
 )(App);
